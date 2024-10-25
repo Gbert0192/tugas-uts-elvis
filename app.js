abf652e129fa6eebf1621c794361f906b7ea6616
Gbert0192 <gtiofandy@gmail.com>
2024-10-25 12:04:25 +0700

    routing + login page done!

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
-const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+
 const app = express();
 const productsRoutes = require("./routes/products"); // Import routes
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(expressLayouts);
 
 app.get("/", (req, res) => {
-  res.render("index"); // Merender views/index.ejs
+  res.render("login", { layout: "partials/mainLogin", title: "Login Page" }); // Merender views/index.ejs
 });
 
 app.use("/products", productsRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
